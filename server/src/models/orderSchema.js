9da70404d1f27b0ef0be64b5c0326c2c8cce2941
Gonzalo Damiano <gdamiano@mi9retail.com>
2018-07-25 17:35:38 -0300

    Se corrigio el esquema de order

--- server/src/models/orderSchema.js
+++ server/src/models/orderSchema.js
@@ -8,10 +8,10 @@ var Orders = new Schema({
 	id : objectId,
 	store_id: {type: objectId, ref: 'Stores' },
 	users: [{ 
-		user_id: {type: objectId, ref: 'Users' },
+		userName: {type: String },
 		food: [{
-			food_id: {type: String, require: true},
-			description: {type: String, require: true},
+			food_id: {type: objectId, require: true},
+			foodName: {type: String, require: true},
 			quantity: {type: Number, require: true},
 			price: {type: Number, require: true}
 		 	}]
